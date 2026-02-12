92cc109e33ea8f4b60b701c3bcdd699706daf2e3
Choi-seungmin09 <seungmin0609@naver.com>
2026-02-12 21:28:59 +0900

    python-study

---
+++ day01.py
@@ -0,0 +1,33 @@
+print("hello 승민")
+name = "승민"
+age = 24
+
+print(name)
+print(age)
+
+a = 10
+b= 3
+
+print(a + b)
+print(a * b)
+
+name = input("이름을 입력하세요")
+print("안녕하세요", name)
+
+score = int(input("점수를 입력하세요: "))
+if score >= 80:
+    print("좋은 자세입니다 !")
+else:
+    print("자세 교정이 필요합니다")
+
+for i in range(5):
+    print("운동 반복 횟수:", i)
+
+name = input("이름 : ")
+age = int(input("나이: "))
+
+if age >= 20:
+    print(name, "님은 성인입니다")
+else:
+    print(name, "님은 미성년자입니다")
+
