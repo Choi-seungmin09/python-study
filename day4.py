519a15428788880be8d5e524d66247da8bb94d07
Choi-seungmin09 <seungmin0609@naver.com>
2026-02-15 23:06:28 +0900

    python basic/day4.py

---
+++ day4.py
@@ -0,0 +1,22 @@
+age = 20
+
+if age >= 20:
+    print("성인입니다. ")
+else:
+    print("미성년자입니다. ")
+
+score = 85
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("F")
+
+for i in range(1,21):
+    if i % 3 == 0:
+        print(i)
+    
