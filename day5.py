dea7470ff301d831844fbdae1ed62ad056829175
Choi-seungmin09 <seungmin0609@naver.com>
2026-02-16 23:17:25 +0900

    basic

---
+++ day5.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5]
+
+for n in numbers:
+    if n % 2 == 0:
+        print(n)
+
+numbers = []
+
+numbers.append(10)
+numbers.append(20)
+
+print(numbers)
+
+numbers = [3, 7, 9, 2]
+
+print (sum(numbers))
